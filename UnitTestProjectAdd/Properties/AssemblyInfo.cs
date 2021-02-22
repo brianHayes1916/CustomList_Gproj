63419e1af8caab6035f6c3b3989cdafe13ae3afb
Brian Hayes <briancampbellhayes@icloud.com>
2021-02-22 10:54:17 -0600

    The main class to hold all the methods and the test class for the add method have been created but are empty.

---
+++ UnitTestProjectAdd/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectAdd")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectAdd")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51e9b95f-e6f1-4884-941f-b301bf3985e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectAdd/UnitTestAdd.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProjectAdd
+{
+    [TestClass]
+    public class UnitTestAdd
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
