bcb76a72e08b88288bd3a325c0f28b75462058e3
Brian Hayes <briancampbellhayes@icloud.com>
2021-02-25 15:36:25 -0600

    First four tests for the overload Operator + done.

---
+++ UnitTestProjectAdd/UnitTest+Overload.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace UnitTestProjectAdd
+{
+    [TestClass]
+    public class UnitTest_Overload
+    {
+        [TestMethod]
+        public void PlusOverload_Item_CorrectCount()
+        {
+            //arrange
+            CustomMadeList<int> customMadeList = new CustomMadeList<int>();
+            customMadeList.Add(1);
+            CustomMadeList<int> customMadeList2 = new CustomMadeList<int>();
+            customMadeList2.Add(2);
+            int expected = 2;
+            int actual;
+            //act
+            CustomMadeList<int> customMadeList3 = customMadeList2 + customMadeList;
+            actual = customMadeList3.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PlusOverload_Item_CorrectCapacity()
+        {
+            //arrange
+            CustomMadeList<int> customMadeList = new CustomMadeList<int>();
+            customMadeList.Add(1);
+            CustomMadeList<int> customMadeList2 = new CustomMadeList<int>();
+            customMadeList2.Add(2);
+            int expected = 4;
+            int actual;
+            //act
+            CustomMadeList<int> customMadeList3 = customMadeList2 + customMadeList;
+            actual = customMadeList3.Capacity;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PlusOverload_Item_InCorrectOrder()
+        {
+            //arrange
+            CustomMadeList<int> customMadeList = new CustomMadeList<int>();
+            customMadeList.Add(1);
+            CustomMadeList<int> customMadeList2 = new CustomMadeList<int>();
+            customMadeList2.Add(2);
+            int expected = 1;
+            int actual;
+            //act
+            CustomMadeList<int> customMadeList3 = customMadeList2 + customMadeList;
+            actual = customMadeList3[1];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PlusOverload_Item_ListOneUnchanged()
+        {
+            //arrange
+            CustomMadeList<int> customMadeList = new CustomMadeList<int>();
+            customMadeList.Add(1);
+            CustomMadeList<int> customMadeList2 = new CustomMadeList<int>();
+            customMadeList2.Add(2);
+            int expected = 1;
+            int actual;
+            //act
+            CustomMadeList<int> customMadeList3 = customMadeList2 + customMadeList;
+            actual = customMadeList[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PlusOverload_Item_
+    }
+}
